b04fac778ca7d9284ec8a809ccd197b257e27e39
brianm85 <mitcho85@hotmail.com>
2015-09-03 14:23:33 +0100

    java bit shifting
    college bit shifting examples

---
+++ bitsd.java
@@ -0,0 +1,16 @@
+package bobs;
+
+public class bitsd {
+
+	public static void main(String[] args) {
+		System.out.println(12 & 3);
+		System.out.println(3|9);
+		System.out.println(-2^4);
+		System.out.println(4<<2);
+		System.out.println(~4>>>3);
+		System.out.println(7>>1);
+		
+
+	}
+
+}
